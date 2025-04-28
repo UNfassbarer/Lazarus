c09605ec3c2f28ebbc53f61030f7afeefb4aef03
Unfassbarer <141145010+UNfassbarer@users.noreply.github.com>
2025-04-28 10:53:07 +0200

    new

---
+++ Notendurschnit/backup/unit1.pas
@@ -0,0 +1,85 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    B_Exit: TButton;
+    B_Berechnen: TButton;
+    E_Ausgabe: TEdit;
+    E_Eingabe: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    L_Scroll: TLabel;
+    ScrollBar1: TScrollBar;
+    procedure Button1Click(Sender: TObject);
+    procedure B_ExitClick(Sender: TObject);
+    procedure E_EingabeKeyPress(Sender: TObject; var Key: char);
+    procedure FormCreate(Sender: TObject);
+    procedure ScrollBar1Change(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  Durschschnitt : real;
+  Counter: real;
+  Notes: real;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.B_ExitClick(Sender: TObject);
+begin
+  ShowMessage('Das Programm wurde am ' + DateToStr(Date) + ' um ' + TimeToStr(Time) + ' Uhr beendet');
+  Application.Terminate;
+end;
+
+procedure TForm1.E_EingabeKeyPress(Sender: TObject; var Key: char);
+begin
+  IF Key = Chr(13) Then
+  begin
+  Counter:= Counter +1;
+  Notes:= Notes + StrToFloat(E_Eingabe.Caption);
+  Durschschnitt:= Notes/Counter;
+  E_Ausgabe.Caption:= FloatToStrF(Durschschnitt, ffFixed, 7, ScrollBar1.Position);
+  E_Eingabe.clear;
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+E_Eingabe.Clear;
+E_Ausgabe.Clear;
+Counter:=0;
+Notes:=0;
+end;
+
+procedure TForm1.ScrollBar1Change(Sender: TObject);
+begin
+L_Scroll.Caption:=IntToStr(ScrollBar1.Position);
+end;
+
+end.
+
